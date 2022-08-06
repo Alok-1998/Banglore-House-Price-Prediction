99554db8e24f017ef7cde210753170039e52f955
Alok <gupta.alok45@gmail.com>
2022-08-07 00:14:20 +0530

    first commit

---
+++ server/server.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+import util
+app = Flask(__name__)
+
+@app.route('/get_location_names')
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-control-allow-origin', '*')
+    return response
+    return "hi"
+
+
+if __name__=="__main__":
+    print("starting python flask server for home price prediction...")
+    app.run()
+
+
+
